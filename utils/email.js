5372612809cd56920bbcbcec3a7a7922905b48e9
balarcrens <balarcrens@gmail.com>
2026-05-18 14:37:53 +0530

    fix email.js

--- utils/email.js
+++ utils/email.js
@@ -2,10 +2,7 @@ const nodemailer = require('nodemailer');
 
 const sendEmail = async (options) => {
     const transporter = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 587,
-        secure: false,
-        family: 4, // FORCE IPv4
+        service: 'gmail',
         auth: {
             user: process.env.SMTP_USER,
             pass: process.env.SMTP_PASS,
